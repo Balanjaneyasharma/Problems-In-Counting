d54d24bd01b09d93e3bbffb43b4859184ff3ad61
Balu Shrama <69688211+Balanjaneyasharma@users.noreply.github.com>
2022-05-13 17:44:42 +0530

    Update DivideArrayIntoEqualPairs.cpp

--- DivideArrayIntoEqualPairs.cpp
+++ DivideArrayIntoEqualPairs.cpp
@@ -13,3 +13,18 @@ public:
         return true;
     }
 };
+
+// without using extra space ......
+
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        
+        for(int i = 1 ; i < nums.size() ; i +=2){
+            if(nums[i]!=nums[i-1]) return false;
+        }
+        
+        return true;
+    }
+};
